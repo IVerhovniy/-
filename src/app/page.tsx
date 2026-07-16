6a4e6a6249a7e9e3d56a41c6e5c51fb8b9e84a70
Александр Никитин <cgbreaking@gmail.com>
2026-07-16 21:42:06 +0300

    fix: добавлено пространство для скролла на главной

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,12 @@ export default function Home() {
   return (
     <>
       <CustomCursor />
-      <main className="flex-1 flex flex-col">
+      <main className="flex-1 flex flex-col relative">
         <LandingHero />
+        {/* Временный блок для скролла, чтобы увидеть параллакс-эффект */}
+        <div className="h-[150vh] w-full bg-transparent flex flex-col items-center pt-32">
+          <p className="text-white/50 font-medium">Продолжение страницы (Шаг 3)...</p>
+        </div>
       </main>
     </>
   );
